fd88715045f946e346f75d1f20c98c6b7aec5ec5
LeoR <leorsky77@gmail.com>
2026-09-01 22:14:54 +0300

    learn: add verb to be

--- main.py
+++ main.py
@@ -48,3 +48,27 @@ My journey to English B2
 # - It
 # - We
 # - They
+
+
+# 01.09.2026
+# 0.3 Verb to be
+#
+# Изучил глагол to be и его формы в настоящем времени.
+#
+# Формы:
+# - I am — я есть / я являюсь
+# - You are — ты / вы есть
+# - He is — он есть
+# - She is — она есть
+# - It is — оно / это есть
+# - We are — мы есть
+# - They are — они есть
+#
+# Практика:
+# - I am
+# - You are
+# - He is
+# - She is
+# - It is
+# - We are
+# - They are
